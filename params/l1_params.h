96f84ed7be5fa8ac30bf356e36358eda8dc8975d
snyder <>
2004-03-15 23:20:09 +0000

    upd

---
+++ params/l1_params.h
@@ -0,0 +1,11 @@
+/* 
+        L1_PARAMS.H;
+         Created           : 13-OCT-1993 by Richard V. Astur
+*/
+
+#define L15BUF       	5				/* buffer size */
+#define NL1PHIL 		32				/* # of phi trigger towers */
+#define NL1ETAL     	20				/* # of eta trigger towers on North */
+#define NULL_FADC_OFF 	NL1PHIL*NL1ETAL /* offset to nonexistant EM towers */
+ 
+
